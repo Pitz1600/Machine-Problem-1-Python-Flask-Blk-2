38ac1592254cd21615ac11ae437d79196984f4fd
sctsandino <joma.enrique.up@phinmaed.com>
2025-12-05 14:52:48 +0800

    another 2

--- name.py
+++ name.py
@@ -2,5 +2,5 @@ name = "Pagasartonga, Peter R."
 print(f"{name}")
 print ("Estrada Matthew Cymon S.")
 print("Brian Isaac M. Villegas")
-print("Shierwin Carl T. Sandino")
 print("Francis I. Canto")
+print("Sandino, Shierwin Carl T.")
